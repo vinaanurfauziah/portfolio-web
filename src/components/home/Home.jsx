0a7a5678373521f6088d61c2998920158a7c14db
vinaanurfauziah <vinaanurfauziah@gmail.com>
2024-07-30 17:52:19 +0700

    komponen home

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,21 +2,24 @@ import React from 'React';
 import './home.css';
 import Social from './Social';
 import Data from './Data';
+import ScrollDown from './ScrollDown';
 
 const Home = () => {
-    return (
-        <section className="home section" id="home">
-            <div className="home__container container grid">
-                <div className="home__content grid">
-                    <Social />
+  return (
+    <section className='home section' id='home'>
+      <div className='home__container container grid'>
+        <div className='home__content grid'>
+          <Social />
 
-                     <div className="home___img"></div> 
+          <div className='home__img'></div>
 
-                     <Data />  
-                </div>
-            </div>
-        </section>
-    )
-}    
+          <Data />
+        </div>
+
+        <ScrollDown />
+      </div>
+    </section>
+  );
+};
 
 export default Home;
