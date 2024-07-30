c5088c970ede33ae439670a22569c729a475ec05
vinaanurfauziah <vinaanurfauziah@gmail.com>
2024-07-30 14:14:39 +0700

    first commit

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Social = () => {
+    return (
+        <div className="home__social">
+            <a href="https://www.instagram.com/vinanurfzh/" className="home__social-icon" target="_blank">
+            <i className="uil uil-instagram"></i>
+            </a>
+
+            <a href="https://www.linkedin.com/in/vinanurfauziahr/" className="home__social-icon" target="_blank">
+            <i className="uil uil-linkedin-alt"></i>
+            </a>
+
+            <a href="https://github.com/vinaanurfauziah/" className="home__social-icon" target="_blank">
+            <i className="uil uil-github-alt"></i>
+            </a>
+
+        </div>
+    )
+}
+
+export default Social
