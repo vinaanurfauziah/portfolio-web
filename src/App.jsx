a92df1b679b852e1330be92dcf83220c3e80f94f
vinaanurfauziah <vinaanurfauziah@gmail.com>
2024-08-10 09:33:30 +0700

    footer component

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Skills from './components/skills/Skills';
 import Services from './components/services/Services';
 import Qualification from './components/qualification/Qualification';
 import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
 
 const App = () => {
   return (
@@ -20,6 +21,7 @@ const App = () => {
       <Services />
       <Qualification />
       <Contact />
+      <Footer />
     </main>
     </>
   )
