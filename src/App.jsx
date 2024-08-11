bd8db1e4ed801438fd85014488a9643d059e0f90
vinaanurfauziah <vinaanurfauziah@gmail.com>
2024-08-11 10:02:54 +0700

    portfolio component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from './components/home/Home';
 import About from './components/about/About';
 import Skills from './components/skills/Skills';
 import Services from './components/services/Services';
+import Work from './components/work/Work';
 import Qualification from './components/qualification/Qualification';
 import Contact from './components/contact/Contact';
 import Footer from './components/footer/Footer';
@@ -21,6 +22,7 @@ const App = () => {
         <Skills />
         <Services />
         <Qualification />
+        <Work />
         <Contact />
       </main>
 
